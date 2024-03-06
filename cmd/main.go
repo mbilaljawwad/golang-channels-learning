a4926796b2a30f89477bbee3e1e22fe1f6949908
Muhammad Bilal Jawwad <m.bilal.jawwad@gmail.com>
2024-03-06 20:36:55 +0500

    channel select completed

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/mbilaljawwad/golang-channels-learning/internal/buffered_channel"
+import "github.com/mbilaljawwad/golang-channels-learning/internal/channel_select"
+
+// import "github.com/mbilaljawwad/golang-channels-learning/internal/buffered_channel"
 
 // import "github.com/mbilaljawwad/golang-channels-learning/internal/channel"
 
@@ -9,5 +11,8 @@ func main() {
 	// channel.ExecuteExample()
 
 	// Buffered channel example
-	buffered_channel.ExecuteExample()
+	// buffered_channel.ExecuteExample()
+
+	// Channel Select statement example
+	channel_select.ExecuteExample()
 }
--- internal/channel_select/channel_select.go
+++ internal/channel_select/channel_select.go
@@ -1,5 +1,21 @@
 package channel_select
 
-func ExecuteExample() {
+import "fmt"
 
+func ExecuteExample() {
+	ninja1, ninja2 := make(chan string), make(chan string)
+
+	go captainElect(ninja1, "Ninja 1")
+	go captainElect(ninja2, "Ninja 2")
+
+	select {
+	case message := <-ninja1:
+		fmt.Println(message)
+	case message := <-ninja2:
+		fmt.Println(message)
+	}
+}
+
+func captainElect(ninja chan string, message string) {
+	ninja <- message
 }
